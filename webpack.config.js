5c7746f9d189daffa5d6a1a2c6073c4353f6fdf3
david30-maker <95672514+david30-maker@users.noreply.github.com>
2023-02-28 19:56:57 +0100

    Add webpack project

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+ module.exports = {
+   entry: {
+     index: './src/index.js'
+    
+   },
+   devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
+   output: {
+     filename: 'bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+     clean: true,
+   },
+   module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+   mode: 'development'
+ };
